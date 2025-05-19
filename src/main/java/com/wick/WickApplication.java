90aa5b7442eb273294ba2b76d4670bcc6b9daee4
kristinadd <kristina@productboard.com>
2025-05-19 08:55:37 +0200

    Init

---
+++ src/main/java/com/wick/WickApplication.java
@@ -0,0 +1,13 @@
+package com.wick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WickApplication.class, args);
+	}
+
+}
